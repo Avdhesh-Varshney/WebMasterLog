2e2fccee653a9331ca982de162ee6118376dd3a7
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-01-10 20:05:33 +0530

    Changing File Path

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // First Dice Random Number:
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var path1 = "../Images/Computer" + randomNumber1 + ".PNG";
+var path1 = "./Images/Computer" + randomNumber1 + ".PNG";
 document.firstElementChild.lastElementChild.querySelectorAll("img")[0].setAttribute("src", path1);
 
 // Second Dice Random Number:
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var path2 = "../Images/Dice" + randomNumber2 + ".PNG";
+var path2 = "./Images/Dice" + randomNumber2 + ".PNG";
 document.firstElementChild.lastElementChild.querySelectorAll("img")[1].setAttribute("src", path2);
 
 // Checking Condition and showing Results
