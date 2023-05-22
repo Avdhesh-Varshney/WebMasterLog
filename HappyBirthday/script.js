0e5392dfe066d696b16dfbc89aaa251375fa135a
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-05-22 11:47:04 +0530

    Wishing Happy Birthday
    Wish Happy birthday 🎂 though this web page in the Stylish & Programming way. ❤️
    Make it's birthday year so priceless. 💖

---
+++ HappyBirthday/script.js
@@ -0,0 +1,9 @@
+function opencard() {
+    let cover = document.getElementById('cover');
+    cover.className = 'open-card';
+}
+
+function closecard() {
+    let cover = document.getElementById('cover');
+    cover.className = '';
+}
