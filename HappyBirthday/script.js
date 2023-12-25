5d2fc4ff34a73685adce4edd969beb72f5af26d3
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-12-26 00:09:36 +0530

    Refine-Web-Projects
    I have done refining of all the web projects which are only designed by me.

--- HappyBirthday/script.js
+++
@@ -1,9 +0,0 @@
-function opencard() {
-    let cover = document.getElementById('cover');
-    cover.className = 'open-card';
-}
-
-function closecard() {
-    let cover = document.getElementById('cover');
-    cover.className = '';
-}
