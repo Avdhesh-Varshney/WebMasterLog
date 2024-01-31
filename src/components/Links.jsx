374215b2870a0f0cfe728fc29f2109ac3af1e17c
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-01-31 14:08:33 +0530

    remove-css-projects
    Removed the directory of CSS Projects as it has no use in future. These projects can come under Front-end-Projects directory.

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
 // Importing Icons 
-import { FaHome, FaAngular, FaCss3, FaReact, FaVuejs } from "react-icons/fa";
+import { FaHome, FaAngular, FaReact, FaVuejs } from "react-icons/fa";
 import { TbBrandNextjs } from "react-icons/tb";
 import { FaNode } from "react-icons/fa6";
 import { IoLogoJavascript } from "react-icons/io5";
 import { MdLaptopWindows } from "react-icons/md";
 
-const categories = ['', 'angular', 'css', 'frontend', 'next', 'node', 'react', 'vanilla', 'vue'];
+const categories = ['', 'angular', 'frontend', 'next', 'node', 'react', 'vanilla', 'vue'];
 const icons = {
   '': <FaHome className="dashboard" />,
   'angular': <FaAngular className="angular" />,
-  'css': <FaCss3 className="css" />,
   'frontend': <MdLaptopWindows className="frontend" />,
   'next': <TbBrandNextjs className="next" />,
   'node': <FaNode className="node" />,
@@ -22,14 +21,12 @@ const icons = {
 
 const routesName = (category) => {
   if (category === '') return 'Dashboard';
-  else if (category === 'css') return 'CSS';
   else if (category === 'frontend') return 'Front End';
   return category[0].toUpperCase() + category.slice(1) + ' JS';
 };
 
 const techName = (category) => {
   if(category === '') return 'Dashboard';
-  else if(category === 'css') return 'CSS-Projects';
   else if(category === 'frontend') return 'Front-end-Projects';
   return category[0].toUpperCase() + category.slice(1) + '-JS-Projects';
 }
--- src/components/Pages/MainPage.jsx
+++ src/components/Pages/MainPage.jsx
@@ -51,10 +51,10 @@ const MainPage = (props) => {
 		<>
 			<h1 className='text-end my-2 mx-3'>{`${getName(category)} Projects`}</h1>
 			<div className="d-flex justify-content-end my-2 mx-3">
-				<button type="button" className="btn btn-success mx-1" onClick={() => handleTagClick('Basic')}>Easy</button>
-				<button type="button" className="btn btn-warning mx-1" onClick={() => handleTagClick('Intermediate')}>Medium</button>
-				<button type="button" className="btn btn-danger mx-1" onClick={() => handleTagClick('Advanced')}>Hard</button>
-				<button type="button" className="btn btn-info mx-1" onClick={() => handleTagClick('All')}>All</button>
+				<button type="button" className={`btn btn${tag !== 'Basic'? '-outline': ''}-success mx-1`} onClick={() => handleTagClick('Basic')}>Easy</button>
+				<button type="button" className={`btn btn${tag !== 'Intermediate'? '-outline': ''}-warning mx-1`} onClick={() => handleTagClick('Intermediate')}>Medium</button>
+				<button type="button" className={`btn btn${tag !== 'Advanced'? '-outline': ''}-danger mx-1`} onClick={() => handleTagClick('Advanced')}>Hard</button>
+				<button type="button" className={`btn btn${tag !== 'All'? '-outline': ''}-info mx-1`} onClick={() => handleTagClick('All')}>All</button>
 			</div>
 
 			<ProjectCards projectsData={projectsData} tech={getTech(category)} />
