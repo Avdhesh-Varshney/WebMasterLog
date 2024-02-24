a4ac410823b4c7307c4ca79a8dff816a897c1747
sristy17 <paulsristy07@gmail.com>
2024-02-24 17:08:48 +0530

    WeatherApp using reactjs

---
+++ React-JS-Projects/Basic/weather-widget/src/App.jsx
@@ -0,0 +1,15 @@
+import WeatherApp from './WeatherApp'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <div className='app'>
+     <WeatherApp/>
+     </div>
+    </>
+  )
+}
+
+export default App
