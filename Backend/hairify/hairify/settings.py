686532c1e56688be708426b087211349040421cb
Subharthi Hazra <92659226+subharthihazra@users.noreply.github.com>
2024-03-30 01:29:57 +0530

    cors origin backend env (#14)
    * homepage
    
    * dashboard frontend
    
    * report frontend
    
    * front end context and uis stuffs added (big pr)
    
    * Update components.json
    
    * fix of bug apstropie
    
    * cors origin added at backend

--- Backend/hairify/hairify/settings.py
+++ Backend/hairify/hairify/settings.py
@@ -186,7 +186,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = str(os.getenv("CORS_LINKS")).split(",")
 
 
 CLOUDINARY = {
