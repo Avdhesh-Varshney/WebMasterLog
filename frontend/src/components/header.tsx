2b312fd8cc22a9b47297d30f19e59df04d3c07cb
Subharthi Hazra <92659226+subharthihazra@users.noreply.github.com>
2024-03-30 11:08:08 +0530

    gfgff (#34)
    * prompt chng
    
    * report form fix && pvt route chat
    
    * landing
    
    * fix report file up
    
    * txt cng
    
    * djfd

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -7,11 +7,11 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <Link  href='/'>
-    <div className="fixed h-[65px] flex justify-between w-full bg-[rgba(255,255,255,0.3)] z-50 backdrop-blur-md headerbar shadow-[rgba(0,0,0,0.1)_0_1px_10px]">
-      <Logo />
-      <Userbuttons />
-    </div>
+    <Link href="/">
+      <div className="fixed h-[65px] flex justify-between w-full bg-[rgba(255,255,255,0.3)] z-50 backdrop-blur-md headerbar shadow-[rgba(0,0,0,0.1)_0_1px_10px]">
+        <Logo />
+        <Userbuttons />
+      </div>
     </Link>
   );
 }
@@ -24,39 +24,51 @@ function Logo() {
   );
 }
 function Userbuttons() {
-  const {authState} = useDataContext()
+  const { authState } = useDataContext();
 
-  const handleLogout = () =>{
-    localStorage.clear()
-    window.location.reload()
-  }
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
 
   return (
     <div className="flex h-full mx-2 gap-2">
-      {authState === 'loggedin'  ? 
-      <HeaderButton text="Logout" url="" onClickFunction={handleLogout} />:  <HeaderButton text="Login" url="/login" /> }
-      {authState === 'loggedin' &&  <HeaderButton text="Dashboard" url="/dashboard" />   }
+      {authState === "loggedin" ? (
+        <HeaderButton text="Logout" url="" onClickFunction={handleLogout} />
+      ) : (
+        <HeaderButton text="Login" url="/login" />
+      )}
+      {authState === "loggedin" && (
+        <HeaderButton text="Dashboard" url="/dashboard" />
+      )}
       {/* <HeaderButton text="Login" url="/login" /> */}
-      
     </div>
   );
 }
-function HeaderButton({ text, url, onClickFunction }: { text: string; url: string, onClickFunction? : ()=>any }) {
+function HeaderButton({
+  text,
+  url,
+  onClickFunction,
+}: {
+  text: string;
+  url: string;
+  onClickFunction?: () => any;
+}) {
   const router = useRouter();
 
-  const handleOnClick = () =>{
-    if(onClickFunction !== undefined){
-      onClickFunction()
+  const handleOnClick = () => {
+    if (onClickFunction !== undefined) {
+      onClickFunction();
+    } else {
+      router.push(url);
     }
-    else{
-      router.push(url)
-    }
-  }
+  };
 
   return (
     <button
       onClick={handleOnClick}
       className=" no-underline text-black"
+      key={text}
     >
       <div className=" text-xl py-2 px-4 border-2 border-black hover:bg-[rgba(255,255,255,0.5)] my-2 transition-all">
         {text}
