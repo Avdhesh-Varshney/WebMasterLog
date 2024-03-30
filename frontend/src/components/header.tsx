ceb55acd0b430e509f00aed1170b880bd468d4e9
Afeefuddin <afeefud2004din@gmail.com>
2024-03-30 10:14:33 +0530

    minor changes

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -3,13 +3,16 @@ import Image from "next/image";
 import icon from "../assets/icon.png";
 import { useRouter } from "next/navigation";
 import { useDataContext } from "@/context/dataContext";
+import Link from "next/link";
 
 export default function Header() {
   return (
+    <Link  href='/'>
     <div className="fixed h-[65px] flex justify-between w-full bg-[rgba(255,255,255,0.3)] z-50 backdrop-blur-md headerbar shadow-[rgba(0,0,0,0.1)_0_1px_10px]">
       <Logo />
       <Userbuttons />
     </div>
+    </Link>
   );
 }
 function Logo() {
@@ -32,8 +35,9 @@ function Userbuttons() {
     <div className="flex h-full mx-2 gap-2">
       {authState === 'loggedin'  ? 
       <HeaderButton text="Logout" url="" onClickFunction={handleLogout} />:  <HeaderButton text="Login" url="/login" /> }
+      {authState === 'loggedin' &&  <HeaderButton text="Dashboard" url="/dashboard" />   }
       {/* <HeaderButton text="Login" url="/login" /> */}
-      <HeaderButton text="Dashboard" url="" />
+      
     </div>
   );
 }
