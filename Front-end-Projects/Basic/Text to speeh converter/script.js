ef31ffc9725c33d1c0c0af9b6271e54452ec34ec
Priya Pandey <priya.pandeyy321@gmail.com>
2024-05-14 18:43:55 +0530

    git'n a sticker

---
+++ Front-end-Projects/Basic/Text to speeh converter/script.js
@@ -0,0 +1,20 @@
+let speech = new SpeechSynthesisUtterance();
+console.log('speech',speech);
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+    console.log('voi',voices)
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
