f9b33cb4012712778b62003f1e1bfd00dbdfe301
Rakshit Sisodiya <64471316+Rakshit-gen@users.noreply.github.com>
2024-05-17 08:54:59 +0530

    Update page.tsx

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -147,7 +147,7 @@ function SignupInner() {
                 onClick={handleSignup}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                {!Signingup ? "Signup" : "Signing in..."}
+                {!Signingup ? "Signup" : "Signing Up..."}
               </button>
             </div>
           </form>
