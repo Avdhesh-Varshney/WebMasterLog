dfd62ecad2193a52f96559efd314a465d5fda570
Priya-49 <priyapragada09@gmail.com>
2024-05-25 11:24:05 +0530

    ui of card

--- src/components/Cards/ProjectCards.jsx
+++ src/components/Cards/ProjectCards.jsx
@@ -13,12 +13,13 @@ const ProjectCards = (props) => {
   };
 	return (
 		<div className='projectsCard row row-cols-1 row-cols-md-3 g-4' style={{ margin: '1rem auto', position: 'absolute', right: '0', top: '15%' }}>
-			{projectsData.map((data, index) => (
-				<div className="col" key={index}>
-					<Card key={index} tech={tech} tag={truncateDescription(data.tag, 1)} title={truncateDescription(data.title, 5)} description={truncateDescription(data.description, 20)} />
-				</div>
-			))}
-		</div>
+    {projectsData.map((data, index) => (
+        <div className="col" key={index} style={{ minWidth: '400px',minHeight:'300px' }}> {/* Set a minimum width */}
+            <Card key={index} tech={tech} tag={truncateDescription(data.tag, 1)} title={truncateDescription(data.title, 5)} description={truncateDescription(data.description, 20)} />
+        </div>
+    ))}
+</div>
+
 	)
 }
 
