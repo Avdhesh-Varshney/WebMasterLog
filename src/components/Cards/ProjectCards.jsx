96b3974f03cdaab6e3d53e44e150aed8d519d219
Priya-49 <priyapragada09@gmail.com>
2024-05-25 18:01:30 +0530

    hover effect modified

--- src/components/Cards/ProjectCards.jsx
+++ src/components/Cards/ProjectCards.jsx
@@ -12,9 +12,9 @@ const ProjectCards = (props) => {
     return description;
   };
 	return (
-		<div className='projectsCard row row-cols-1 row-cols-md-3 g-4' style={{ margin: '1rem auto', position: 'absolute', right: '0', top: '15%' }}>
+		<div className='projectsCard row row-cols-1 row-cols-md-3 g-4' style={{margin: '1rem auto', position: 'absolute', right: '0', top: '15%' }}>
     {projectsData.map((data, index) => (
-        <div className="col" key={index} style={{ minWidth: '400px',minHeight:'300px' }}> {/* Set a minimum width */}
+        <div className="col" key={index} style={{ minWidth: '360px',minHeight:'300px',paddingTop:'20px',paddingBottom:'20px' }}> {/* Set a minimum width */}
             <Card key={index} tech={tech} tag={truncateDescription(data.tag, 1)} title={truncateDescription(data.title, 5)} description={truncateDescription(data.description, 20)} />
         </div>
     ))}
