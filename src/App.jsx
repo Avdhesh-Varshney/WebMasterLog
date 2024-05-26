d062786127ced6dd0f478171607aefb42a0454c5
Rohitha Pudu <rohithapudu@gmail.com>
2024-05-26 22:19:11 +0530

    Add toggle theme and pass props

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,33 @@ import Links from './components/Links';
 import MainPage from './components/Pages/MainPage';
 import Footer from './components/Footer/Footer';
 
+
 const categories = ['angular', '', 'frontend', 'next', 'node', 'react', 'vanilla', 'vue'];
 const routes = Links();
 
 function App() {
   const [progress, setProgress] = useState(0);
+  const [theme,setTheme]=useState('light');
+	const toggleTheme = () => {
+		setTheme(theme === 'light' ? 'dark' : 'light');
+	  };
+
 
   return (
     <div>
       <Router>
-        <SideBar routes={routes}>
+        <SideBar routes={routes} theme={theme} toggleTheme={toggleTheme}>
           <LoadingBar height={3} color='#f11946' progress={progress} />
 
           <Routes>
             {categories.map((category) => {
-              return <Route key={category} exact path={`/${category}`} element={<MainPage setProgress={setProgress} key={category} category={category} routes={routes} />} />;
+              return <Route key={category} exact path={`/${category}`} element={<MainPage setProgress={setProgress} key={category} category={category} routes={routes} theme={theme} toggleTheme={toggleTheme}/>} />;
             })}
           </Routes>
         </SideBar>
 
       </Router>
-      <Footer/>
+      <Footer theme={theme}/>
     </div>
   );
 }
