971e57d796466ec1c841ff86b9f73a5b383bbf58
Rohitha Pudu <rohithapudu@gmail.com>
2024-05-26 22:52:06 +0530

    changed default theme to dark

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const routes = Links();
 
 function App() {
   const [progress, setProgress] = useState(0);
-  const [theme,setTheme]=useState('light');
+  const [theme,setTheme]=useState('dark');
 	const toggleTheme = () => {
 		setTheme(theme === 'light' ? 'dark' : 'light');
 	  };
