725d4725e8bd273334a38693a22544b72daf96bb
Saimanjari777 <psmanjari777@gmail.com>
2024-06-05 22:12:35 +0530

    format code

--- Vanilla-JS-Projects/Basic/Expense-Tracker/script.js
+++ Vanilla-JS-Projects/Basic/Expense-Tracker/script.js
@@ -3,22 +3,12 @@ const expenseForm =
 const expenseList = 
 	document.getElementById("expense-list"); 
 const totalAmountElement = 
-	document.getElementById("total-amount"); 
-
-// Initialize expenses array from localStorage 
+	document.getElementById("total-amount");  
 let expenses = 
 	JSON.parse(localStorage.getItem("expenses")) || []; 
-
-// Function to render expenses in tabular form 
 function renderExpenses() { 
-
-	// Clear expense list 
-	expenseList.innerHTML = ""; 
-
-	// Initialize total amount 
+	expenseList.innerHTML = "";  
 	let totalAmount = 0; 
-
-	// Loop through expenses array and create table rows 
 	for (let i = 0; i < expenses.length; i++) { 
 		const expense = expenses[i]; 
 		const expenseRow = document.createElement("tr"); 
@@ -28,8 +18,6 @@ function renderExpenses() {
 	<td class="delete-btn" data-id="${i}">Delete</td> 
 	`; 
 		expenseList.appendChild(expenseRow); 
-
-		// Update total amount 
 		totalAmount += expense.amount; 
 	}  
 	totalAmountElement.textContent = 
@@ -37,8 +25,6 @@ function renderExpenses() {
 	localStorage.setItem("expenses", 
 		JSON.stringify(expenses)); 
 } 
-
-// Function to add expense 
 function addExpense(event) { 
 	event.preventDefault(); 
 	const expenseNameInput = 
@@ -59,33 +45,17 @@ function addExpense(event) {
 		name: expenseName, 
 		amount: expenseAmount, 
 	}; 
-
-	// Add expense to expenses array 
 	expenses.push(expense); 
-
-	// Render expenses 
 	renderExpenses(); 
 } 
-
-// Function to delete expense 
 function deleteExpense(event) { 
 	if (event.target.classList.contains("delete-btn")) { 
-
-		// Get expense index from data-id attribute 
 		const expenseIndex = 
-			parseInt(event.target.getAttribute("data-id")); 
-
-		// Remove expense from expenses array 
-		expenses.splice(expenseIndex, 1); 
-
-		// Render expenses 
+			parseInt(event.target.getAttribute("data-id"));
+		expenses.splice(expenseIndex, 1);
 		renderExpenses(); 
 	} 
 } 
-
-// Add event listeners 
 expenseForm.addEventListener("submit", addExpense); 
 expenseList.addEventListener("click", deleteExpense); 
-
-// Render initial expenses on page load 
 renderExpenses();
