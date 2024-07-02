1115b9928cf3561dccca2623bdbe1f8985f3164b
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-07-02 20:01:47 +0530

    routing-fixes

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='container my-2'>
+      <h1 className='text-center'>WEB MASTER LOG</h1>
+    </div>
+  )
+}
+
+export default Dashboard;
