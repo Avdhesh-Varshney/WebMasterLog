5556180ae844abcc366de06c8514f85e308f45a1
avi <avineshwarthegreat@gmail.com>
2024-07-06 09:06:08 +0530

    GasGuys Homepage Clone project added

---
+++ Vanilla-JS-Projects/Intermediate/GasGuys-HomePage-Clone/script.js
@@ -0,0 +1,131 @@
+let currentVideoIndex = 0;
+const videos = document.querySelectorAll(".video");
+
+function showVideo(index) {
+  videos.forEach((video, i) => {
+    video.style.display = i === index ? "block" : "none";
+    if (i === index) video.play();
+    else video.pause();
+  });
+}
+
+function nextVideo() {
+  currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+  showVideo(currentVideoIndex);
+}
+
+function previousVideo() {
+  currentVideoIndex = (currentVideoIndex - 1 + videos.length) % videos.length;
+  showVideo(currentVideoIndex);
+}
+
+// Add event listeners to all videos to switch when ended
+videos.forEach((video, index) => {
+  video.addEventListener("ended", () => {
+    nextVideo();
+  });
+});
+
+showVideo(currentVideoIndex);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+// form js
+
+document
+  .getElementById("consultationForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Form submitted!");
+  });
+
+const menu = document.querySelector(".menu1");
+const menuMain = menu.querySelector(".menu-main");
+const goBack = menu.querySelector(".go-back");
+const menuTrigger = document.querySelector(".mobile-menu-trigger");
+const closeMenu = menu.querySelector(".mobile-menu-close");
+let subMenu;
+menuMain.addEventListener("click", (e) => {
+  if (!menu.classList.contains("active")) {
+    return;
+  }
+  if (e.target.closest(".menu-item-has-children")) {
+    const hasChildren = e.target.closest(".menu-item-has-children");
+    showSubMenu(hasChildren);
+  }
+});
+goBack.addEventListener("click", () => {
+  hideSubMenu();
+});
+menuTrigger.addEventListener("click", () => {
+  toggleMenu();
+});
+closeMenu.addEventListener("click", () => {
+  toggleMenu();
+});
+document.querySelector(".menu-overlay").addEventListener("click", () => {
+  toggleMenu();
+});
+function toggleMenu() {
+  menu.classList.toggle("active");
+  document.querySelector(".menu-overlay").classList.toggle("active");
+}
+function showSubMenu(hasChildren) {
+  subMenu = hasChildren.querySelector(".sub-menu");
+  subMenu.classList.add("active");
+  subMenu.style.animation = "slideLeft 0.5s ease forwards";
+  const menuTitle =
+    hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
+  menu.querySelector(".current-menu-title").innerHTML = menuTitle;
+  menu.querySelector(".mobile-menu-head").classList.add("active");
+}
+
+function hideSubMenu() {
+  subMenu.style.animation = "slideRight 0.5s ease forwards";
+  setTimeout(() => {
+    subMenu.classList.remove("active");
+  }, 300);
+  menu.querySelector(".current-menu-title").innerHTML = "";
+  menu.querySelector(".mobile-menu-head").classList.remove("active");
+}
+
+window.onresize = function () {
+  if (this.innerWidth > 991) {
+    if (menu.classList.contains("active")) {
+      toggleMenu();
+    }
+  }
+};
