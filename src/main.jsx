44ca5aa233db0f9106308e55aed18694f59b2519
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-07-13 22:03:02 +0530

    caching-added
    Implement the functionality of caching the data to avoid the api hit again & again on same end point.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import ContextProvider from './contexts/Context';
+import queryClient from './contexts/QueryClient';
+import { QueryClientProvider } from 'react-query';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
