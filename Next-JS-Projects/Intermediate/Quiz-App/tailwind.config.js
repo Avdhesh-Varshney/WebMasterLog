fc28ed7d6ffaa97931095296dbbf60367489f122
Dharshi Balasubramaniyam <139672976+DharshiBalasubramaniyam@users.noreply.github.com>
2024-07-21 00:19:12 +0530

    Quiz app - NEXT.js (#582)
    * Create README.md
    
    * added files
    
    * Update Next.js README.md

---
+++ Next-JS-Projects/Intermediate/Quiz-App/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+};
