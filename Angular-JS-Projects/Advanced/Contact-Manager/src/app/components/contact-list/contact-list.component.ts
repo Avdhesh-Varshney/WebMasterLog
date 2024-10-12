9f1c28d4c888fcb982b3ee846578ecb1d74a6fb7
mawalou14 <awaloum213@gmail.com>
2024-10-13 00:39:03 +0100

    Updated contact Management app UI and styling

--- Angular-JS-Projects/Advanced/Contact-Manager/src/app/components/contact-list/contact-list.component.ts
+++ Angular-JS-Projects/Advanced/Contact-Manager/src/app/components/contact-list/contact-list.component.ts
@@ -1,55 +1,74 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ContactService } from '../../services/contact.service';
-import { Contact } from '../../models/contact';
-import { CommonModule } from '@angular/common';
-import { EditContactComponent } from '../edit-contact/edit-contact.component';
-import { FormsModule } from '@angular/forms';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from "@angular/core";
+import { ContactService } from "../../services/contact.service";
+import { Contact } from "../../models/contact";
+import { CommonModule } from "@angular/common";
+import { EditContactComponent } from "../edit-contact/edit-contact.component";
 
 @Component({
-  selector: 'app-contact-list',
+  selector: "app-contact-list",
   standalone: true,
-  imports: [CommonModule, EditContactComponent, FormsModule],
-  templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  imports: [CommonModule, EditContactComponent],
+  templateUrl: "./contact-list.component.html",
+  styleUrl: "./contact-list.component.css",
 })
-export class ContactListComponent implements OnInit {
-
+export class ContactListComponent implements OnInit, OnChanges {
   contacts: Contact[] = [];
-  editContact: Contact = {id: "", name: "", email: "", phone: ""};
-  searchQuery: string = "";
+  editContact: Contact = { id: "", name: "", email: "", phone: "" };
+  @Input() searchQuery: string = "";
   filteredContacts: Contact[] = [];
 
   toggleEditContact(contact: Contact): void {
     this.editContact = contact;
-    console.log("currently the edit contact is: ", this.editContact)
+    console.log("currently the edit contact is: ", this.editContact);
   }
 
   ngOnInit(): void {
-    this.getAllTasks()
+    this.getAllTasks();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["searchQuery"]) {
+      this.searchContacts();
+    }
   }
 
   constructor(private contactService: ContactService) {}
 
   getAllTasks(): void {
-    this.contactService.getAllContacts().subscribe(res => {
-      this.contacts = res.sort((a, b) => a.name.localeCompare(b.name))
-      this.filteredContacts = this.contacts
-    }, err => {
-      console.log(err)
-    })
+    this.contactService.getAllContacts().subscribe(
+      (res) => {
+        this.contacts = res.sort((a, b) => a.name.localeCompare(b.name));
+        this.filteredContacts = this.contacts;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
-  deleteContact(contact: Contact):void {
-    this.contactService.deleteContact(contact).subscribe(res => {
-      console.log(res)
-      window.location.reload()
-    }, err => {
-      console.log(err)
-    })
+  deleteContact(contact: Contact): void {
+    this.contactService.deleteContact(contact).subscribe(
+      (res) => {
+        console.log(res);
+        window.location.reload();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
   searchContacts(): void {
-    this.filteredContacts = this.contacts
-    .filter(contact => contact.name.toLowerCase().includes(this.searchQuery.toLowerCase()) || contact.phone.includes(this.searchQuery))
+    this.filteredContacts = this.contacts.filter(
+      (contact) =>
+        contact.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        contact.phone.includes(this.searchQuery)
+    );
   }
 }
