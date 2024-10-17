1603366e473da8b80662e7871f4a1db96bc9f597
Mouhamadou Awalou <97563838+mawalou14@users.noreply.github.com>
2024-10-17 15:00:55 +0100

    Update UI and Styling for Contact Management App (#889)
    * Updated contact Management app UI and styling
    
    * Update Angular/Advanced/Contact App readme.md
    
    Updated the screenshots found in the readme.md file of the contact management angular application
    
    * Added toastr notification & delete unecessary files
    
    * Restored mandatory screenshot.webp file
    
    * update screenshots

--- Angular-JS-Projects/Advanced/Contact-Manager/src/app/app.component.ts
+++ Angular-JS-Projects/Advanced/Contact-Manager/src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
+import { ContactListComponent } from "./components/contact-list/contact-list.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, ContactListComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'contact-manager';
+  title = "contact-manager";
+
+  searchQuery: string = "";
+
+  onSearchChanged(value: string): void {
+    this.searchQuery = value;
+  }
 }
