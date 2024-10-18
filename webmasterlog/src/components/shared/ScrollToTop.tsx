842e96d4b0cb9554d99e5609082ae5f9bc01e368
Shariq <109452033+Shariq2003@users.noreply.github.com>
2024-10-18 15:42:02 +0530

    Added Smooth Scrolling in B2T (#942)

--- webmasterlog/src/components/shared/ScrollToTop.tsx
+++ webmasterlog/src/components/shared/ScrollToTop.tsx
@@ -14,11 +14,13 @@ const ScrollToTop = () => {
     }
   };
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+  const smoothScrollToTop = () => {
+    const scrollY = window.pageYOffset;
+    const scrollStep = Math.max(10, Math.floor(scrollY / 20)); 
+    if (scrollY > 0) {
+      window.scrollBy(0, -scrollStep);
+      requestAnimationFrame(smoothScrollToTop);
+    }
   };
 
   useEffect(() => {
@@ -30,7 +32,7 @@ const ScrollToTop = () => {
     <div>
       {isVisible && (
         <Button
-          onClick={scrollToTop}
+          onClick={() => requestAnimationFrame(smoothScrollToTop)}
           className="fixed bottom-4 right-4 p-3 bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition"
         >
           ↑
