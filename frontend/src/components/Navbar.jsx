6cddad184e61f655f07ab40ce79b9e087ecff041
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-01 14:01:00 +0000

    update: styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,25 +13,25 @@ const Navbar = () => {
                 <input
                     type="text"
                     placeholder="Search"
-                    className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
+                    className="w-full md:w-auto bg-gray-100 p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12 outline-none"
                 />
                 <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
             </div>
 
             <div className="flex items-center gap-3 md:gap-6 ml-auto">
-                <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center" onClick={() => setSearchBoxVisibility(!searchBoxVisibility)}>
+                <button className="md:hidden bg-gray-100 w-12 h-12 rounded-full flex items-center justify-center" onClick={() => setSearchBoxVisibility(!searchBoxVisibility)}>
                     <i className="fi fi-rr-search text-xl"></i>
                 </button>
 
-                <Link to="/editor" className="hidden md:flex gap-2 text-dark-grey hover:text-black hover:bg-grey p-3 px-4 opacity-75">
+                <Link to="/editor" className="hidden md:flex items-center gap-2 text-gray-700 hover:text-black hover:bg-gray-200 p-3 px-4 rounded-lg transition">
                     <i className="fi fi-rr-file-edit"></i>
                     <p>Write</p>
                 </Link>
 
-                <Link className="btn-dark py-2" to="/api/signin">
+                <Link className="bg-black text-white py-2 px-5 rounded-full hover:bg-gray-800 transition" to="/api/signin">
                     Sign In
                 </Link>
-                <Link className="btn-light py-2 hidden md:block" to="/api/signup">
+                <Link className="bg-gray-200 text-gray-800 py-2 px-5 rounded-full hidden md:block hover:bg-gray-300 transition" to="/api/signup">
                     Sign Up
                 </Link>
             </div>
