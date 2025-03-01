a0be116f4821cde19e3e8b3c41ac637a433c957a
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-01 19:15:52 +0000

    setup: routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="login" element={<h1>Login Page</h1>} />
+          <Route path="signup" element={<h1>SignUp Page</h1>} />
+        </Route>
+      </Routes>
     </>
   )
 }
