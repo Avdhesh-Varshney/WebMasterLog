d22c6df3d51c28f04384e30d6b227bba8c614b7f
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-02 05:49:18 +0000

    fixes: search-responsiveness

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,11 +10,12 @@ const Navbar = () => {
                 <Link to="/" className="flex-none w-10">
                     <img src="logo.png" alt="" className="w-full" />
                 </Link>
-                <div className={"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " + (searchBoxVisibility ? "hide" : "show")}>
+                <div className={"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-gray-200 py-4 px-[5vw] md:border-0 md:relative md:inset-0 md:p-0 md:w-auto " + (searchBoxVisibility ? "show" : "hidden md:block")}
+                >
                     <input
                         type="text"
                         placeholder="Search"
-                        className="w-full md:w-auto bg-gray-100 p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12 outline-none"
+                        className="w-full md:w-auto bg-gray-100 p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
                     />
                     <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
                 </div>
