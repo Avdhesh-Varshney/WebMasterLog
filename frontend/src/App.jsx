0f64bb9a6c2c7183a42e73bb7de453a086577277
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-03 20:13:11 +0530

    [PLT-26] User Authorization Login/Signup Page (#1092)
    * setup: routing
    
    * fixes: search-responsiveness
    
    * add on user signup/login page
    
    * fixes operators & update page-animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import UserAuthForm from "./pages/UserAuthForm";
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="login" element={<UserAuthForm type="login" />} />
+          <Route path="signup" element={<UserAuthForm type="signup" />} />
+        </Route>
+      </Routes>
     </>
   )
 }
