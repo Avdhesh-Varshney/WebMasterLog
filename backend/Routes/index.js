a7a4db509dcbf35745493f787d9a5ad559d2b7cc
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-06 19:11:07 +0530

    created: get-upload-url route

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import authRoutes from './api/auth.routes.js';
+import mediaUploadRoute from './api/get-upload-url.routes.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.use('/media', mediaUploadRoute);
 
 export default router;
