c758936934f112473aaf7293357df03ed64dc653
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-08 18:02:37 +0530

    created: get-trending projects route

--- backend/Controllers/project.controller.js
+++ backend/Controllers/project.controller.js
@@ -68,7 +68,7 @@ export const createProject = async (req, res) => {
         })
 }
 
-export const getProject = async (req, res) => {
+export const getProjects = async (req, res) => {
 
     let maxLimit = 5;
 
@@ -77,6 +77,21 @@ export const getProject = async (req, res) => {
         .sort({ "publishedAt": -1 })
         .select("project_id title des banner tags activity publishedAt -_id")
         .limit(maxLimit)
+        .then(projects => {
+            return res.status(200).json({ projects });
+        })
+        .catch(err => {
+            return res.status(500).json({ error: err.message });
+        })
+}
+
+export const trendingProjects = async (req, res) => {
+
+    Project.find({ draft: false })
+        .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
+        .sort({ "activity.total_read": -1, "activity.total_likes": -1, "publishedAt": -1 })
+        .select("project_id title publishedAt -_id")
+        .limit(5)
         .then(projects => {
             return res.status(200).json({ projects });
         })
